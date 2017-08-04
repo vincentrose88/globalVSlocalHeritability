0d60a213c15d9cb7cff26975d66124f019ea1e79
vincentrose88 <vincentrose88@gmail.com>
2017-08-04 14:58:17 +0200

    added keep-allele-order flag to threshold run sim.

--- runSim.thresholds.sh
+++ runSim.thresholds.sh
@@ -10,7 +10,7 @@ for admix in 0.5 0.6 0.7
 do
     for pop in inuit danes
     do
-	plink19 --allow-no-sex --bfile geno/GreenlandSiblings.autosome.geno.0.01.withAdmix --extract trunc.sim/sim.$sim.truncSNPs --keep plink/${admix}.$pop --recode A --out plink/sim.$sim.${admix}.$pop
+	plink19 --keep-allele-order --allow-no-sex --bfile geno/GreenlandSiblings.autosome.geno.0.01.withAdmix --extract trunc.sim/sim.$sim.truncSNPs --keep plink/${admix}.$pop --recode A --out plink/sim.$sim.${admix}.$pop
 	Rscript ./creating.own.pheno.R plink/sim.$sim.${admix}.$pop.raw trunc.sim/sim.$sim.truncSNPs.withBetas plink/sim.$sim.all.frq trunc.sim/sim.$sim.${admix}.$pop.genoSum
     done
 done
@@ -18,7 +18,7 @@ done
 for admix in 0.8 0.9 0.99
 do
     pop=inuit
-    plink19 --allow-no-sex --bfile geno/GreenlandSiblings.autosome.geno.0.01.withAdmix --extract trunc.sim/sim.$sim.truncSNPs --keep plink/${admix}.$pop --recode A --out plink/sim.$sim.${admix}.$pop
+    plink19 --keep-allele-order --allow-no-sex --bfile geno/GreenlandSiblings.autosome.geno.0.01.withAdmix --extract trunc.sim/sim.$sim.truncSNPs --keep plink/${admix}.$pop --recode A --out plink/sim.$sim.${admix}.$pop
     Rscript ./creating.own.pheno.R plink/sim.$sim.${admix}.$pop.raw trunc.sim/sim.$sim.truncSNPs.withBetas plink/sim.$sim.all.frq trunc.sim/sim.$sim.${admix}.$pop.genoSum	    
 done
 
